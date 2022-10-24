a30afad4347adf9800a2360348c0bc8e4fccf643
jeswi2 <jeswimathewm@gmail.com>
2022-10-25 02:18:51 +0530

    se

---
+++ src/app/api.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+ 
+   adduser=(data:any)=>{
+    return this.http.post("http://localhost:8080/adduser",data)
+   }
+
+   signin=(data:any)=>{
+return this.http.post("http://localhost:8080/signin",data)
+   }
+  }
+
+
+
+
+
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-signin',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class SigninComponent implements OnInit {
 
-  constructor(private myrouter:Router) { }
+  constructor(private myRouter:Router,private myapi:ApiService) { }
 
   email=""
   password=""
@@ -19,6 +20,21 @@ export class SigninComponent implements OnInit {
       "password":this.password
     }
 console.log(data)
+this.myapi.signin(data).subscribe(
+  (response:any)=>{
+    if (response.length>0)
+    {
+
+      localStorage.setItem("stored_name",response[0].name)
+      localStorage.setItem("stored_id",response[0].id)
+      this.myRouter.navigate(["/tweet"])
+
+    }
+    else{
+      alert("invalid credentails")
+    }
+  }
+)
 }
 
   
